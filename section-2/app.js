d5eaae015aed56daa09481c97621012fe9d1a73d
Baxter <davidf.martinez.br@gmail.com>
2023-10-20 15:32:25 -0500

    Excercise 1, 2, 3, 4

---
+++ section-2/app.js
@@ -0,0 +1,28 @@
+//? Excercise #1 - print Hello Word
+// console.log('Hello World');
+
+//? Excercise #2 - Variable
+let a = 10;
+const b = 20;
+
+//* this cannot be done
+//b = 30
+
+let x = a + b;
+console.log('x = ' + x);
+
+//? Excercise #3 - clg
+console.log('a:', a);
+
+console.log({ a });
+
+let c = 'Spiderman',
+	d = 'Messi';
+
+console.table({ a, b, c, d, x });
+
+const greetings = 'Hola ' + d;
+console.log(greetings)
+
+//? Excercise #4 - Debugging
+c = 'say hi'
